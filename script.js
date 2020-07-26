aed0d6d6fc1f74b100662d69dccfcf54609f8ad2
Lee-changgeun <halfwing1998@gmail.com>
2020-07-27 00:05:13 +0900

    Chang date and month and prev and next

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+//window.addEventListener('DOMContentLoaded',main);
+let today=new Date();
+let date = new Date();
+todayYear = today.getFullYear();
+todayMonth = today.getMonth()+1;
+document.getElementsByClassName('ToMonth').innerHTML=todayMonth;
+let firstDate= new Date(today.getFullYear(),today.getMonth(),1);
+let lastDate = new Date(today.getFullYear(),today.getMonth()+1,0);
+let day= firstDate.getDay();
+let Calendar = document.getElementsByClassName('WholeDate');
+let week=Math.ceil(lastDate.getDate()/7)+1;
+//console.log(day);
 
 
-// document.getElementById('CurrentMonth').value =new Date().toISOString().slice(0,7);
-//document.getElementsByClassName('ViewMonth').value=new Date.prototype.getMonth();
-let table = (document.getElementsByClassName('WholeDate'))[0];
-let arr = document.getElementsByTagName('td');
-let today= new Date();
-let todayYear = today.getFullYear();
-let todayMonth = today.getMonth()+1;
-//let td=table.getElementsByClassName('WholeDate');
-//console.log(ViewMonth);
-let CheckMonth=document.getElementsByClassName('ToMonth')[0];
-console.log(CheckMonth);
-console.log(document.getElementsByClassName('ToMonth')[0]);
+/*function prevCal(){
 
-CheckMonth.innerHTML=todayMonth;
-CheckMonth.textContent=todayMonth;
+}
+
+function main(){
+   // console.log("1");
+   let table = (document.getElementsByClassName('WholeDate'))[0];
+   let arr= document.getElementsByTagName('td');
+   let todayMonth = today.getMonth()+1;
+   let CheckMonth = document.getElementsByClassName('ToMonth')[0];
+   let CheckDate = document.getElementsByClassName('ViewDate');
+   CheckMonth.innerHTML=todayMonth;
+   CheckMonth.textContent=todayMonth+"월";
+   
+    //console.log('DOM fully loaded and parsed');
+
+   // console.log("2");
+   
+
+}*/
 
 
-//let tbody= document.getElementsByTagName('tbody');
-//CheckMonth.innerHTML=todayMonth;
-//CheckMonth.textContent=todayMonth;
-//document.getElementsByClassName('ToMonth').innerHTML=todayMonth;
-//let 
-//for(let i = 0; i<arr.length; i++){
- //   td[i].textContent=i;
-//}
-console.log(today);
-console.log(todayYear);
-console.log(todayMonth);
-//console.log(tbody);
+
