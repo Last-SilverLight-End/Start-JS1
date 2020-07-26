417426fdf372b1557e54dfd22d87377f31818138
Lee-changgeun <halfwing1998@gmail.com>
2020-07-26 17:36:35 +0900

    putting date and month is being needed

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
  document.getElementById('CurrentMonth').value =new Date().toISOString().slice(0,7);
  document.getElementsByClassName('ViewMonth').value=new Date.prototype.getMonth();
+let table = (document.getElementsByClassName('WholeDate'))[0];
+let tr=table.getElementsByClassName();
+console.log(ViewMonth);
+
+let tbody= document.getElementsByTagName('tbody');
+
+console.log(tbody);
