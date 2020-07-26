e45bf18e397d7c70c8c33098004e2efa1e10e155
Lee-changgeun <halfwing1998@gmail.com>
2020-07-26 17:07:03 +0900

    달력 중반 내용 이게 나라냐

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 
-    
-console.log('sejong');
+
+ document.getElementById('CurrentMonth').value =new Date().toISOString().slice(0,7);
+ document.getElementsByClassName('ViewMonth').value=new Date.prototype.getMonth();
