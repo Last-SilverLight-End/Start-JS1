0457ab752d6e24335b3d32459a16f50bbe1413a3
Lee-changgeun <halfwing1998@gmail.com>
2020-07-28 14:07:18 +0900

    month using possible rest is day

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
-//window.addEventListener('DOMContentLoaded',main);
-let today=new Date();
+window.addEventListener('DOMContentLoaded',buildCal);
+
+
+let today = new Date();
 let date = new Date();
-todayYear = today.getFullYear();
-todayMonth = today.getMonth()+1;
-document.getElementsByClassName('ToMonth').innerHTML=todayMonth;
-let firstDate= new Date(today.getFullYear(),today.getMonth(),1);
-let lastDate = new Date(today.getFullYear(),today.getMonth()+1,0);
-let day= firstDate.getDay();
-let Calendar = document.getElementsByClassName('WholeDate');
-let week=Math.ceil(lastDate.getDate()/7)+1;
-//console.log(day);
 
 
-/*function prevCal(){
+function prevCal(){
+    today = new Date(today.getFullYear(),today.getMonth()-1,today.getDate());
+    buildCal();
+}
+
+function nextCal(){
+    today =  new Date(today.getFullYear(),today.getMonth()+1,today.getDate());
+    buildCal();
+}
+
+function buildCal(){
+    let doMonth = new Date(today.getFullYear(),today.getMonth(),1); // 첫째날
+    let lastDate = new Date(today.getFullYear(),today.getMonth()+1,0); // 이번 마지막 날
+    let tbCal = document.getElementById("WholeCalendar");
+    let tbCalendarYM = document.getElementById("tbCalendarYM");
+
+    tbCalendarYM.textcontent=today.getFullYear()+"년"+(today.getMonth()+1) +"월";
+    tbCalendarYM.innerHTML=today.getFullYear()+"년"+(today.getMonth()+1) +"월";
+    
+    console.log(tbCalendarYM);
 
 }
 
-function main(){
-   // console.log("1");
-   let table = (document.getElementsByClassName('WholeDate'))[0];
-   let arr= document.getElementsByTagName('td');
-   let todayMonth = today.getMonth()+1;
-   let CheckMonth = document.getElementsByClassName('ToMonth')[0];
-   let CheckDate = document.getElementsByClassName('ViewDate');
-   CheckMonth.innerHTML=todayMonth;
-   CheckMonth.textContent=todayMonth+"월";
-   
-    //console.log('DOM fully loaded and parsed');
 
-   // console.log("2");
-   
 
-}*/
+/*function buildCal(){
+
+    let leftDays=7;
+    let setDays=1;
+    for(let i=1; i<week; i++){
+
+        let row = Calendar.insertRow(0);
+        while(day!=0){
+            row.insertCell().innerHTML="";
+            day-=1;
+            leftDays-=1;
+        }
+        let nextMonthDate=1;
+        while(leftDays!=0){
+            if(setDays >lastDate.getDate()){
+                row.insertCell().innerHTML = nextMonthDate;
+                leftDays-=1;
+                nextMonthDate+=1;
+            }
+            else{
+                row.insertCell().innerHTML=setDays;
+                setDays+=1;
+                leftDays-=1;
+            }
+        }
+        leftDays=7;
+    }
+}
+buildCal();*/
+
 
 
 
